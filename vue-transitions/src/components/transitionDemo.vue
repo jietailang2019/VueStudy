21516cdeab998d10650560a065a0781b1800bafe
Airen <weihua.lwh@alibaba-inc.com>
2019-02-14 23:06:56 +0800

    add base transition demo

---
+++ vue-transitions/src/components/transitionDemo.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="demo">
+        <div class="toggle toggle--text">
+			<input type="checkbox" id="toggle--text" class="toggle--checkbox">
+			<label class="toggle--btn" for="toggle--text" data-label-on="show"  data-label-off="hidden" @click="toggle"></label>
+		</div>
+
+        <transition name="fade">
+            <p v-if="show">Hello W3cplus (^_^) !!!</p>
+        </transition>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'transitionDemo',
+        data () {
+            return {
+                show: true
+            }
+        },
+        methods: {
+            toggle() {
+                this.show = !this.show
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity .3s;
+}
+
+.fade-enter,
+.fade-leave-to {
+    opacity: 0;
+}
+
+/* default style*/
+.toggle {
+	display: block;
+	text-align: center;
+	margin-top: 40px;
+    margin-bottom: 40px;
+    user-select: none;
+}
+.toggle--checkbox {
+    display: none;
+}
+.toggle--btn {
+    display: block;
+	margin: 0 auto;
+    font-size: 1.4em;
+    transition: all 350ms ease-in;
+}
+.toggle--btn:hover {
+	cursor: pointer;
+}
+.toggle--btn {
+    position: relative;
+    width: 320px;
+    height: 70px;
+    text-transform: capitalize;
+    font-weight: bold;
+    border-radius: 40px;
+    border: 1px solid #8c8c8c;
+    border-right: 2px solid #8c8c8c;
+    box-shadow: 
+        inset 90px 0 10px rgba(255, 255, 255, 0.6), 
+        inset -3px 3px 18px -2px #8c8c8c, 
+        0 0 15px #ccc, 
+        0px -5px 16px 1px #ddd, 
+        -5px -5px 16px 1px #ddd, 
+        5px -5px 16px 1px #ddd, 
+        -5px -5px 16px 1px #ddd, 
+        5px -5px 16px 1px #ddd, 
+        17px 9px 0 -5px rgba(255, 255, 255, 0.6), 
+        -3px 6px 12px -7px #000, 
+        0 0 0 14px #fff, 
+        0 0 0 15px #999;
+}
+.toggle--btn::before,
+.toggle--btn::after {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+}
+.toggle--btn::before {
+    content: attr(data-label-on);
+    left: 15%;
+    color: #db4c00;    
+}
+.toggle--btn::after {
+    content: attr(data-label-off);
+    right: 15%;
+    color: #999;
+}
+.toggle--checkbox:checked + .toggle--btn {
+    border-left: 2px solid #8c8c8c;
+    border-right: 1px solid #8c8c8c;
+    box-shadow: inset 90px 0 10px rgba(255, 255, 255, 0), inset 3px 3px 18px -2px #8c8c8c, 0 0 15px #ccc, 0px -5px 16px 1px #ddd, -5px -5px 16px 1px #ddd, 5px -5px 16px 1px #ddd, -5px -5px 16px 1px #ddd, 5px -5px 16px 1px #ddd, -17px 9px 0 -5px rgba(255, 255, 255, 0.6), 3px 6px 12px -7px #000, 0 0 0 14px #fff, 0 0 0 15px #999;
+}
+.toggle--checkbox:checked + .toggle--btn::before {
+    color: #999;
+}
+.toggle--checkbox:checked + .toggle--btn::after {
+    color: #db4c00;
+}
+
+p {
+    font-size: 3em;
+}
+</style>
