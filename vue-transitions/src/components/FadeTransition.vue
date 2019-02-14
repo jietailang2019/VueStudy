622748b6214d7c1981d6683384cc31c6ad272874
Airen <weihua.lwh@alibaba-inc.com>
2019-02-14 23:47:52 +0800

    add fade transition component

---
+++ vue-transitions/src/components/FadeTransition.vue
@@ -0,0 +1,23 @@
+<template>
+    <transition name="fade">
+        <slot />
+    </transition>
+</template>
+
+<script>
+    export default {
+        name: 'FadeTransition'
+    }
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity .3s;
+}
+
+.fade-enter,
+.fade-leave-to {
+    opacity: 0;
+}
+</style>
